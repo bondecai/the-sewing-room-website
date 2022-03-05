9efd733ce9f1ddff2d1d98f7317af31429733915
bondecai <35945575+bondecai@users.noreply.github.com>
2022-03-05 16:59:31 -0600

    Routed Navbar links to different pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React, { Component } from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { LinkContainer } from "react-router-bootstrap";
+import { Route, BrowserRouter as Router, useRouteMatch, Link, Routes } from "react-router-dom";
+
 import Navigation from './Navigation'
+import Home from './Home'
+import About from './About'
 
 function App() {
-  return (
-    <Navigation />
-  )
+    return (
+        <Navigation />
+    )
 }
 
-export default App
+export default App;
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,8 +1,29 @@
-import Navbar from 'react-bootstrap/Navbar'
+import React, { Component } from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Route, Routes, BrowserRouter as Router, Link, Outlet } from 'react-router-dom'
+
+import About from './About'
+import Home from './Home'
 
 function Navigation() {
     return (
-        <Navbar bg="dark" variant="dark"></Navbar>
+        <Router>
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="home">The Sewing Room</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link as={Link} to="/">Home</Nav.Link>
+                    <Nav.Link as={Link} to="/about">About Us</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+        </Routes>
+        </Router>
     )
 }
 
